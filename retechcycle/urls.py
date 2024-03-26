11562210da83578e80d7f18007f8f77d952a4b1f
Md. Meherub Hossain Yemon <100033382+mhyemon95@users.noreply.github.com>
2024-03-27 00:51:39 +0600

    Update urls.py

--- retechcycle/urls.py
+++ retechcycle/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ecom import views
-from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
