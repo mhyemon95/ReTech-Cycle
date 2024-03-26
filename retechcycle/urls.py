5a8450e36c301141a243838f345cd724d6c83f23
Md. Meherub Hossain Yemon <100033382+mhyemon95@users.noreply.github.com>
2024-03-27 00:19:00 +0600

    Update urls.py

--- retechcycle/urls.py
+++ retechcycle/urls.py
@@ -18,17 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from ecom import views
 from django.contrib.auth.views import LoginView,LogoutView
-#from django.contrib.auth.views import LoginView,LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view,name=''), # home page view
     path('afterlogin', views.afterlogin_view,name='afterlogin'),
-    
-    # path('adminclick', views.adminclick_view),
-    # path('adminlogin', LoginView.as_view(template_name='adminlogin.html'),name='adminlogin'),
-    
-    
-    path('customersignup', views.customer_signup_view),
     path('customerlogin', LoginView.as_view(template_name='customerlogin.html'),name='customerlogin'),
 ]
